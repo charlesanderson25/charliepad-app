2a5e9f7ed801f7e91858202aecbbd90fc1b60e71
charlesanderson25 <charles_spfc@hotmail.com>
2023-06-17 17:42:12 -0300

    Changes style

--- App.tsx
+++ App.tsx
@@ -38,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#404040",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
