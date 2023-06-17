e9b0300af4106b7da26397f4e180c50cf575b85e
charlesanderson25 <charles_spfc@hotmail.com>
2023-06-17 17:41:58 -0300

    Add style

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,41 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { AntDesign } from "@expo/vector-icons";
 
+const textBanner = {
+  title: "Charlie",
+  titleTwo: "Pad",
+};
+
 const Banner = () => {
   return (
-    <View>
-      <AntDesign name="book" size={24} color="black" />
-      <Text>Icone</Text>
+    <View style={styles.containerViewBanner}>
+      <AntDesign style={styles.icon} name="book" size={24} color="black" />
+      <Text style={styles.textTitle}>
+        {textBanner.title}
+        <Text style={styles.textTitleTwo}>{textBanner.titleTwo}</Text>{" "}
+      </Text>
     </View>
   );
 };
 
 export default Banner;
+
+const styles = StyleSheet.create({
+  containerViewBanner: {
+    flex: 1,
+    flexDirection: "row",
+  },
+
+  icon: {
+    backgroundColor: "#f9004d",
+  },
+
+  textTitle: {
+    color: "#fff",
+  },
+
+  textTitleTwo: {
+    color: "#f9004d",
+  },
+});
