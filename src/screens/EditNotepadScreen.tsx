8dc0ebe5da0a4279ccb40c110ebf5d885c4fbb3e
charlesanderson25 <charles_spfc@hotmail.com>
2023-06-18 19:13:22 -0300

    Add <Text>

--- src/screens/EditNotepadScreen.tsx
+++ src/screens/EditNotepadScreen.tsx
@@ -1,7 +1,12 @@
+import { Text } from "react-native";
 import { View } from "react-native";
 
 const EditNotepadScreen = () => {
-  return <View>EditNotepadScreen</View>;
+  return (
+    <View>
+      <Text>EditNotepadScreen</Text>
+    </View>
+  );
 };
 
 export default EditNotepadScreen;
