8d6bc5c1d2f0f48a3ac18903b50f40b7c68802b0
charlesanderson25 <charles_spfc@hotmail.com>
2023-06-18 19:13:01 -0300

    Add Navigation

--- App.tsx
+++ App.tsx
@@ -1,17 +1,30 @@
+import { NavigationContainer } from "@react-navigation/native";
+import * as React from "react";
 import "react-native-gesture-handler";
 import { StyleSheet, StatusBar, SafeAreaView } from "react-native";
 import Banner from "./src/components/Banner";
-import { CreateNotepadScreen } from "./src/screens/CreatNotepadScreen";
-import { EditNotepadScreen } from "./src/screens/CreatNotepadScreen";
-import { HomeScreen } from "./src/screens/CreatNotepadScreen";
-import { ListNotepadScreen } from "./src/screens/CreatNotepadScreen";
+import CreateNotepadScreen from "./src/screens/CreatNotepadScreen";
+import EditNotepadScreen from "./src/screens/CreatNotepadScreen";
+import HomeScreen from "./src/screens/CreatNotepadScreen";
+import ListNotepadScreen from "./src/screens/CreatNotepadScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar />
-      <Banner />
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView style={styles.container}>
+        <StatusBar />
+        <Banner />
+      </SafeAreaView>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="CreateNotepad" component={CreateNotepadScreen} />
+        <Stack.Screen name="EditNotepad" component={EditNotepadScreen} />
+        <Stack.Screen name="ListNotepad" component={ListNotepadScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
