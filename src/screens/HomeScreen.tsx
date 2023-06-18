4bc132abafa63d309433ff030d43de10d9b29f8b
charlesanderson25 <charles_spfc@hotmail.com>
2023-06-18 19:13:26 -0300

    Add <Text>

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,18 @@
-import { View } from "react-native";
+import { Button } from "react-native";
+import { View, Text } from "react-native";
 
-const HomeScreen = () => {
-  return <View>HomeScreen</View>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Button
+        title="Ver Notepads"
+        onPress={() => {
+          navigation.navigate("ListNotepad");
+        }}
+      />
+      <Text>HomeScreen</Text>
+    </View>
+  );
 };
 
 export default HomeScreen;
