63f7cd352387e3d575314dffc5639c83ee68d8bc
charlesanderson25 <charles_spfc@hotmail.com>
2023-06-19 18:07:13 -0300

    Add component

---
+++ src/screens/ViewNotepadScreen.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { View } from "react-native";
+
+const ViewNotepadScreen = () => {
+  return (
+    <View>
+      <Text>ViewNotepadScreen</Text>
+    </View>
+  );
+};
+
+export default ViewNotepadScreen;
