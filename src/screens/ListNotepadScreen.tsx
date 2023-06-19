11945381bd5888e93a0bc8a9de381354b04860d4
charles <charles_spfc@hotmail.com>
2023-06-19 20:50:06 -0300

    Add useState, useEffect and async function

--- src/screens/ListNotepadScreen.tsx
+++ src/screens/ListNotepadScreen.tsx
@@ -1,7 +1,27 @@
 import { Text } from "react-native";
 import { View } from "react-native";
+import { useEffect, useState } from "react";
+import api from "../../api";
 
-const ListNotepadScreen = () => {
+const initialNotepads = {
+  count: 0,
+  notepads: [],
+};
+
+const ListNotepadScreen = ({ navigation, route }) => {
+  const [notepadList, setNotepadList] = useState(initialNotepads);
+
+  async function loadNotepads() {
+    const response = await api.get("/notepads");
+    setNotepadList(response.data);
+  }
+
+  useEffect(() => {
+    const unsubscribe = navigation.adListenner("focus", () => {});
+
+    return unsubscribe;
+  }, []);
+
   return (
     <View>
       <Text>ListNotepadScreen</Text>
