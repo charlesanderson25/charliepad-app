1db7289b4b40a52bd3f2b494db143036536f4655
charlesanderson25 <charles_spfc@hotmail.com>
2023-06-23 19:18:08 -0300

    Add CardPad component

---
+++ src/components/CardPad.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+const CardPad = styled.View`
+  border-radius: 8px;
+  background-color: white;
+  padding: 8px;
+  margin: 12px;
+  elevation: 12;
+`;
+
+export default CardPad;
