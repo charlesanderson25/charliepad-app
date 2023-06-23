8e34703f734bf62b0738a92fd32ba451a8ff5edf
charlesanderson25 <charles_spfc@hotmail.com>
2023-06-23 19:18:18 -0300

    Add Container

--- src/components/NotepadItem.tsx
+++ src/components/NotepadItem.tsx
@@ -1,14 +1,31 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import styled from "styled-components/native";
 
 const Title = styled.Text`
-  font-size: 20px;
+  font-size: 18px;
   font-weight: bold;
   // /* font-family: JosefinSans-Regular; */
 `;
 
-const NotepadItem = ({ title }) => {
-  return <Title>{title}</Title>;
+const SubTitle = styled.Text`
+  font-size: 14px;
+  color: #444;
+`;
+
+const Container = styled.View`
+  padding-horizontal: 10px;
+  padding-vertical: 8px;
+  border-bottom-width: 1px;
+  border-bottom-color: #aaa;
+`;
+
+const NotepadItem = ({ title, subtitle }) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <SubTitle>{subtitle}</SubTitle>
+    </Container>
+  );
 };
 
 export default NotepadItem;
