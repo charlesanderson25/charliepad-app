bd17019514c79523e29fb00140116843f43c5fd4
charlesanderson25 <charles_spfc@hotmail.com>
2023-06-24 12:06:24 -0300

    Add onPressNotepadItem

--- src/screens/ListNotepadScreen.tsx
+++ src/screens/ListNotepadScreen.tsx
@@ -1,15 +1,18 @@
-import { Text } from "react-native";
-import { View } from "react-native";
 import { useEffect, useState } from "react";
 import api from "../../api";
 import { FlatList } from "react-native-gesture-handler";
 import NotepadItem from "../components/NotepadItem";
 import CardPad from "../components/CardPad";
+import Toast from "react-native-root-toast";
 
 const initialNotepads = {
   count: 0,
   notepads: [],
 };
+
+function onPressNotepadItem({ title }) {
+  Toast.show(title);
+}
 
 const ListNotepadScreen = ({ navigation, route }) => {
   const [notepadList, setNotepadList] = useState(initialNotepads);
@@ -32,7 +35,9 @@ const ListNotepadScreen = ({ navigation, route }) => {
       <FlatList
         data={notepadList.notepads}
         renderItem={({ item }) => {
-          return <NotepadItem {...item} />;
+          return (
+            <NotepadItem {...item} onPress={() => onPressNotepadItem(item)} />
+          );
         }}
       />
     </CardPad>
