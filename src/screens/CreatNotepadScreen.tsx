ba4ad0ebce90063f44d001296870b8044d6a1144
charlesanderson25 <charles_spfc@hotmail.com>
2023-06-26 17:58:31 -0300

    Add useState

--- src/screens/CreatNotepadScreen.tsx
+++ src/screens/CreatNotepadScreen.tsx
@@ -2,9 +2,23 @@ import styled from "styled-components/native";
 import { TextInput } from "react-native";
 import Button from "../components/Button";
 import Toast from "react-native-root-toast";
+import { useState } from "react";
+
+const initialNotepads = {
+  title: "",
+  subtitle: "",
+  content: "",
+};
+
+const textsCreateNotepad = {
+  titleText: "Digite o Título",
+  subtitleText: "Digite o Subtítulo",
+  contentText: "Digite o conteúdo",
+};
 
 const Container = styled.View`
   margin: 14px;
+  gap: 8px;
 `;
 
 const TextField = styled.TextInput`
@@ -15,9 +29,26 @@ const TextField = styled.TextInput`
 `;
 
 const CreateNotepadScreen = () => {
+  const [title, setTitle] = useState("");
+  const [subTitle, setSubTitle] = useState("");
+  const [content, setContent] = useState("");
+
   return (
     <Container>
-      <TextField placeholder="Digite o Título" />
+      <TextField
+        placeholder={textsCreateNotepad.titleText}
+        onChangeText={setTitle}
+      />
+      <TextField
+        placeholder={textsCreateNotepad.subtitleText}
+        onChangeText={setSubTitle}
+      />
+      <TextField
+        placeholder={textsCreateNotepad.contentText}
+        multiline
+        numberOfLines={6}
+        onChangeText={setContent}
+      />
       <Button
         onPress={() => {
           Toast.show("Fui clicado");
