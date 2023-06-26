c84cfab359b7ef39b8eab90bfd3666dd3d931bb0
charles <charles_spfc@hotmail.com>
2023-06-25 21:04:38 -0300

    Add useEffect, useState, async function

--- src/screens/ViewNotepadScreen.tsx
+++ src/screens/ViewNotepadScreen.tsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react";
 import { Text } from "react-native";
 import { View } from "react-native";
+import Toast from "react-native-root-toast";
+import api from "../../api";
 
-const ViewNotepadScreen = () => {
+const initialNotepads = {
+  id: "",
+  title: "",
+  subtitle: "",
+  content: "",
+  created_at: "",
+};
+
+const ViewNotepadScreen = ({ navigation, route }) => {
+  const notepadId = route.params.id;
+  const [notepad, setNotepad] = useState(initialNotepads);
+
+  async function loadNotepads() {
+    const response = await api.get(`/notepads/${notepadId}`);
+    setNotepad(response.data);
+  }
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {});
+
+    loadNotepads();
+
+    return unsubscribe;
+  }, [notepadId]);
+
   return (
     <View>
-      <Text>ViewNotepadScreen</Text>
+      <Text>{notepad.id}</Text>
+      <Text>{notepad.created_at}</Text>
+      <Text>{notepad.title}</Text>
+      <Text>{notepad.subtitle}</Text>
+      <Text>{notepad.content}</Text>
     </View>
   );
 };
