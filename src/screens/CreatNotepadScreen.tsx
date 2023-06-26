46c389468b73184544bb3af03dc1bd46e749477c
charlesanderson25 <charles_spfc@hotmail.com>
2023-06-26 17:45:07 -0300

    Add Container, TextField, Button

--- src/screens/CreatNotepadScreen.tsx
+++ src/screens/CreatNotepadScreen.tsx
@@ -1,11 +1,31 @@
-import { Text } from "react-native";
-import { View } from "react-native";
+import styled from "styled-components/native";
+import { TextInput } from "react-native";
+import Button from "../components/Button";
+import Toast from "react-native-root-toast";
+
+const Container = styled.View`
+  margin: 14px;
+`;
+
+const TextField = styled.TextInput`
+  border-radius: 8px;
+  background-color: white;
+  padding: 8px;
+  border-width: 1px;
+`;
 
 const CreateNotepadScreen = () => {
   return (
-    <View>
-      <Text>CreateNotepadScreen</Text>
-    </View>
+    <Container>
+      <TextField placeholder="Digite o Título" />
+      <Button
+        onPress={() => {
+          Toast.show("Fui clicado");
+        }}
+      >
+        Enviar
+      </Button>
+    </Container>
   );
 };
 
