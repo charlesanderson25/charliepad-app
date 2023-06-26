9e499dae3f9443c9d98dc6b0429258fef79a1422
charlesanderson25 <charles_spfc@hotmail.com>
2023-06-26 17:44:46 -0300

    Add Button.tsx

---
+++ src/components/Button.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components/native";
+
+const ButtonContainer = styled.TouchableOpacity`
+  background-color: #f9004d;
+  border-radius: 8px;
+  padding-vertical: 8px;
+  padding-horizontal: 16px;
+`;
+
+const ButtonText = styled.Text`
+  color: white;
+  text-transform: uppercase;
+  font-size: 16px;
+  text-align: center;
+  font-weight: bold;
+`;
+
+const Button = ({ children, onPress }) => {
+  return (
+    <ButtonContainer onPress={onPress}>
+      <ButtonText>{children}</ButtonText>
+    </ButtonContainer>
+  );
+};
+
+export default Button;
