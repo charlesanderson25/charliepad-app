45e306d0e6f32193193c7bfc93f37dbb8e9325c4
charles <charles_spfc@hotmail.com>
2023-06-25 21:18:16 -0300

    Add component Title

---
+++ src/components/Title.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+const Title = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  // /* font-family: JosefinSans-Regular; */
+`;
+
+export default Title;
