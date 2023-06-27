28ca6552f53a027f02f53cb10aa4a06a509414d1
charles <charles_spfc@hotmail.com>
2023-06-26 21:23:08 -0300

    Add const notepadId

--- src/screens/EditNotepadScreen.tsx
+++ src/screens/EditNotepadScreen.tsx
@@ -1,10 +1,12 @@
 import { Text } from "react-native";
 import { View } from "react-native";
 
-const EditNotepadScreen = () => {
+const EditNotepadScreen = ({ navigation, route }) => {
+  const notepadId = route.params.id;
+
   return (
     <View>
-      <Text>EditNotepadScreen</Text>
+      <Text>EditNotepadScreen {notepadId}</Text>
     </View>
   );
 };
