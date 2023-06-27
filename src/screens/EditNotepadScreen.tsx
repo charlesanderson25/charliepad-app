431a91145e0b2d4c9c89b6be7021748716c11495
charles <charles_spfc@hotmail.com>
2023-06-26 22:04:52 -0300

    Add useState and loadNotepads

--- src/screens/EditNotepadScreen.tsx
+++ src/screens/EditNotepadScreen.tsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
 import { Text } from "react-native";
 import { View } from "react-native";
 
 const EditNotepadScreen = ({ navigation, route }) => {
   const notepadId = route.params.id;
+  const [title, setTitle] = useState("");
+  const [subtitle, setSubtitle] = useState("");
+  const [content, setContent] = useState("");
+
+  async function loadNotepads() {
+    const response = await api.get(`/notepads/${notepadId}`);
+    const notepad = response.data;
+    setTitle(notepad.title);
+    setSubtitle(notepad.subtitle);
+    setContent(notepad.content);
+  }
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {});
+    return unsubscribe;
+  }, [notepadId]);
 
   return (
     <View>
