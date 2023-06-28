49d00aaf958f7a8aee09234810400c18f7a3bdac
charles <charles_spfc@hotmail.com>
2023-06-28 20:59:08 -0300

    Add onSubmit and changes return

--- src/screens/EditNotepadScreen.tsx
+++ src/screens/EditNotepadScreen.tsx
@@ -2,6 +2,15 @@ import { useEffect, useState } from "react";
 import { Text } from "react-native";
 import { View } from "react-native";
 import api from "../../api";
+import TextField from "../components/TextField";
+import Container from "../components/Container";
+import Button from "../components/Button";
+import Toast from "react-native-root-toast";
+
+const textsEditNotepad = {
+  submitButton: "Atualizar",
+  updateNotepadSucess: "O notepad foi atulizado com sucesso!",
+};
 
 const EditNotepadScreen = ({ navigation, route }) => {
   const notepadId = route.params.id;
@@ -17,15 +26,30 @@ const EditNotepadScreen = ({ navigation, route }) => {
     setContent(notepad.content);
   }
 
+  async function onSubmit() {
+    const response = await api.patch(`/notepads/${notepadId}`, {
+      title,
+      subtitle,
+      content,
+    });
+    Toast.show(textsEditNotepad.updateNotepadSucess);
+    navigation.goBack();
+  }
+
   useEffect(() => {
-    const unsubscribe = navigation.addListener("focus", () => {});
+    const unsubscribe = navigation.addListener("focus", () => {
+      loadNotepads();
+    });
     return unsubscribe;
   }, [notepadId]);
 
   return (
-    <View>
-      <Text>EditNotepadScreen {notepadId}</Text>
-    </View>
+    <Container>
+      <TextField value={title} onChangeText={setTitle} />
+      <TextField value={subtitle} onChangeText={setSubtitle} />
+      <TextField value={content} onChangeText={setContent} />
+      <Button onPress={onSubmit}>{textsEditNotepad.submitButton}</Button>
+    </Container>
   );
 };
 
