cba00d77c3302f83332bd66eda4bdd67b023a120
charles <charles_spfc@hotmail.com>
2023-06-28 20:57:04 -0300

    Add component TextField

---
+++ src/components/TextField.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+const TextField = styled.TextInput`
+  border-radius: 8px;
+  background-color: white;
+  padding: 8px;
+  border-width: 1px;
+`;
+
+export default TextField;
