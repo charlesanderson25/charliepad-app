9090ffa464e7c818b53ad7c1c0fedcaefd55b032
charles <charles_spfc@hotmail.com>
2023-06-28 20:57:52 -0300

    Add ButtonProps

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,30 @@
+// import styled from "styled-components/native";
+
+// const ButtonContainer = styled.TouchableOpacity`
+//   background-color: #f9004d;
+//   border-radius: 8px;
+//   padding-vertical: 8px;
+//   padding-horizontal: 16px;
+// `;
+
+// const ButtonText = styled.Text`
+//   color: white;
+//   text-transform: uppercase;
+//   font-size: 16px;
+//   text-align: center;
+//   font-weight: bold;
+// `;
+
+// const Button = ({ children, onPress, style }) => {
+//   return (
+//     <ButtonContainer onPress={onPress} style={style}>
+//       <ButtonText>{children}</ButtonText>
+//     </ButtonContainer>
+//   );
+// };
+
+// export default Button;
+
 import styled from "styled-components/native";
 
 const ButtonContainer = styled.TouchableOpacity`
@@ -15,7 +42,13 @@ const ButtonText = styled.Text`
   font-weight: bold;
 `;
 
-const Button = ({ children, onPress, style }) => {
+type ButtonProps = {
+  children: React.ReactNode;
+  onPress: () => Promise<void>;
+  style?: any;
+};
+
+const Button: React.FC<ButtonProps> = ({ children, onPress, style }) => {
   return (
     <ButtonContainer onPress={onPress} style={style}>
       <ButtonText>{children}</ButtonText>
