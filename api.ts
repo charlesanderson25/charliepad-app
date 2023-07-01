e4e94198e7a3c45911aeb50fe2f7f96d91f13034
charlesanderson25 <charles_spfc@hotmail.com>
2023-07-01 11:13:14 -0300

    Add globalState

--- api.ts
+++ api.ts
@@ -1,7 +1,23 @@
 import axios from "axios";
+import { useGlobalState } from "./useGlobalState";
+
+const globalState = useGlobalState.getState();
+const setIsLoading = (
+  globalState as { setIsLoading: (isLoading: boolean) => void }
+).setIsLoading;
 
 const api = axios.create({
   baseURL: "https://notepads.eduardovelho.com/",
+});
+
+api.interceptors.request.use((config) => {
+  setIsLoading(true);
+  return config;
+});
+
+api.interceptors.response.use((response) => {
+  setIsLoading(false);
+  return response;
 });
 
 export default api;
