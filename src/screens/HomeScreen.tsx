eb60d041bcdcaae5b109b9b01dfb7ed5ce5abe05
charlesanderson25 <charles_spfc@hotmail.com>
2023-07-02 16:27:31 -0300

    Add MapView

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,17 +1,16 @@
 import { Button } from "react-native";
 import { View, Text } from "react-native";
 import screens from "../../screens.json";
+import MapView from "react-native-maps";
+import { PROVIDER_GOOGLE } from "react-native-maps";
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <Button
-        title="Ver Notepads"
-        onPress={() => {
-          navigation.navigate(screens.listNotepad);
-        }}
+    <View style={{ width: "100%", height: "100%" }}>
+      <MapView
+        style={{ width: "100%", height: "100%" }}
+        provider={PROVIDER_GOOGLE}
       />
-      <Text>HomeScreen</Text>
     </View>
   );
 };
