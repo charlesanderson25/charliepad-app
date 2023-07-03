9ddf283961b0dfbbd9e30cb1c433c019fc56ed35
charles <charles_spfc@hotmail.com>
2023-07-02 21:58:35 -0300

    Add onMarkerPress and notepadsMarkers

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,10 +2,16 @@ import { Button } from "react-native";
 import { View, Text } from "react-native";
 import screens from "../../screens.json";
 import MapView from "react-native-maps";
-import { PROVIDER_GOOGLE } from "react-native-maps";
+import { PROVIDER_GOOGLE, MapMarker } from "react-native-maps";
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
 import Toast from "react-native-root-toast";
+import api from "../../api";
+
+const initialNotepads = {
+  count: 0,
+  notepads: [],
+};
 
 const delta = 0.05;
 
@@ -21,22 +27,55 @@ const initialCordinate = {
 
 const HomeScreen = ({ navigation }) => {
   const [cordinate, setCordinate] = useState(initialCordinate);
+  const [notepadList, setNotepadList] = useState(initialNotepads);
+
   const region = {
     ...cordinate,
     latitudeDelta: delta,
     longitudeDelta: delta,
   };
 
+  const notepadsInMap = notepadList.notepads.filter(
+    (notepad) =>
+      notepad.latitude !== undefined && notepad.longitude !== undefined
+  );
+
+  async function loadNotepads() {
+    const response = await api.get("/notepads", {
+      params: {
+        limit: Infinity,
+      },
+    });
+    setNotepadList(response.data);
+  }
+
   async function loadGeoLocation() {
     const response = await Location.requestForegroundPermissionsAsync();
     const position = await Location.getCurrentPositionAsync();
     setCordinate(position.coords);
-    console.log(position);
+    // console.log(position);
+  }
+
+  function onMarkerPress(notepad) {
+    navigation.navigate(screens.viewNotepad, { id: notepad.id });
   }
 
   useEffect(() => {
     loadGeoLocation();
+    const unsubscribe = navigation.addListener("focus", () => {
+      loadNotepads();
+    });
+
+    return unsubscribe;
   }, []);
+
+  const notepadsMarkers = notepadsInMap.map((notepad) => (
+    <MapMarker
+      key={notepad.id}
+      coordinate={notepad}
+      onPress={() => onMarkerPress(notepad)}
+    />
+  ));
 
   return (
     <View style={{ width: "100%", height: "100%" }}>
@@ -45,7 +84,9 @@ const HomeScreen = ({ navigation }) => {
         showsUserLocation={true}
         style={{ width: "100%", height: "100%" }}
         provider={PROVIDER_GOOGLE}
-      />
+      >
+        {notepadsMarkers}
+      </MapView>
     </View>
   );
 };
